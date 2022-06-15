f884b665d0bec443fe4747efc13bf636a47dd30d
gigyo <gigyo76@gmail.com>
2022-06-15 10:41:52 +0200

    Ej02 lista

---
+++ Ej-02/index.js
@@ -0,0 +1,13 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// - Tu nombre (string)
+// - Tu edad (number)
+// - ¿Eres desarrollador? (boolean)
+// - Tu fecha de nacimiento (Date)
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const persona =
+["Gisela", 45, true, "1976/08/13", 
+    {titulo: "La dama del alba", autor: "Alejandro Cassona", fecha: "2006/06/05", url: "https://www.amazon.es/dama-del-alba-Letras-Hisp%C3%A1nicas/dp/8437604656/ref=sr_1_1?adgrpid=61688008448&gclid=Cj0KCQjwhqaVBhCxARIsAHK1tiNr03QQxbrWDbS0IHurYkbYaeQGZvy59Sh01cYoshVIOV5k-urU3UwaAoPoEALw_wcB&hvadid=601382675450&hvdev=c&hvlocphy=9049031&hvnetw=g&hvqmt=e&hvrand=6616530968385431310&hvtargid=kwd-359027148229&hydadcr=21822_2277548&keywords=la+dama+del+alba+libro&qid=1655280736&sr=8-1"}
+]
+
+console.log(persona);
